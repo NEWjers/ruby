14943d79d07ea0a0e3a7edc85faeb21a6375393d
Serhiy Pityk <serjsonet@gmail.com>
2021-12-04 16:44:59 +0200

    Lab1 OOP version

--- binance.rb
+++ binance.rb
@@ -1,26 +1,40 @@
 require 'open-uri'
 
-url = 'https://www.binance.com/en/markets'
-html = URI.open(url)
+class BinanceReader
+  def initialize(url)
+    @url = url
+  end
 
-require 'nokogiri'
+  def getDocument
+    # url = 'https://www.binance.com/en/markets'
+    html = URI.open(@url)
+    require 'nokogiri'
+    doc = Nokogiri::HTML(html)
+    return doc
+  end
 
-doc = Nokogiri::HTML(html)
-
-tradeItems = doc.xpath('//div[@id="market_trade_list_item"]')
+  def processDoc
+  doc = getDocument
+  tradeItems = doc.xpath('//div[@id="market_trade_list_item"]')
   
-tradeItems.each do |currency|
-  columns = currency.xpath('./div')
-  
-  data = columns.first(2).map{ |column|
-    column.xpath('./div/div')
-      .map{ |element| element.text}
-      .join
-      .gsub(/\s+/, "")
-  }.join(',')
-  
-  open('data.csv', 'a') { |f| 
-    f.puts data
-  }
+  tradeItems.each do |currency|
+    columns = currency.xpath('./div')
+    
+    data = columns.first(2).map{ |column|
+      column.xpath('./div/div')
+        .map{ |element| element.text}
+        .join
+        .gsub(/\s+/, "")
+    }.join(',')
+    
+    open('data.csv', 'a') { |f| 
+      f.puts data
+    }
+    end
+  end
 end
+
+reader = BinanceReader.new('https://www.binance.com/en/markets')
+reader.processDoc
+
 
